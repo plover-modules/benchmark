8c3d20fa19ffa1ae3523f9872228b772ec841ee4
bencode <bencode@163.com>
2016-07-01 19:00:13 +0800

    feat: add test suite for assign

--- benchmark/assign.js
+++ benchmark/assign.js
@@ -1,4 +1,70 @@
 'use strict';
 
 
+const assign = require('plover-util/lib/assign');
 
+const Suite = require('./suite');
+const suite = new Suite();
+
+
+
+const t1kk = 1 * 1000 * 1000;
+
+const o = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10
+};
+
+
+suite.mark('Object.assign', () => {
+  for (let i = 0; i < t1kk; i++) {
+    Object.assign({}, o);
+  }
+});
+
+
+function assignA(des, src) {
+  for (const k in src) {
+    des[k] = src[k];
+  }
+}
+
+suite.mark('assignA', () => {
+  for (let i = 0; i < t1kk; i++) {
+    assignA({}, o);
+  }
+});
+
+
+function assignB(des, src) {
+  const keys = Object.keys(src);
+  for (let i = 0, c = keys.length; i < c; i++) {
+    const key = keys[i];
+    des[key] = src[key];
+  }
+}
+
+
+suite.mark('assignB', () => {
+  for (let i = 0; i < t1kk; i++) {
+    assignB({}, o);
+  }
+});
+
+
+suite.mark('plover-util/lib/assign', function() {
+  for (let i = 0; i < t1kk; i++) {
+    assign({}, o);
+  }
+});
+
+
+suite.report();
---
+++ benchmark/suite.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+/* eslint no-console: 0 */
+
+
+class Suite {
+  constructor() {
+    this.items = [];
+  }
+
+
+  mark(name, fn) {
+    const start = Date.now();
+    const item = {
+      name: name,
+      start: start,
+      cost: -1
+    };
+    this.items.push(item);
+
+    const done = () => {
+      item.cost = Date.now() - start;
+    };
+
+    if (fn) {
+      fn();
+      done();
+    }
+
+    return done;
+  }
+
+
+  report() {
+    for (const item of this.items) {
+      console.log(`${item.name} ${item.cost}\n`);
+    }
+  }
+}
+
+
+module.exports = Suite;
